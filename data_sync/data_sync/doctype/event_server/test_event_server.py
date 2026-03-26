773c1b09a385ad82754b40d18fc72f3f0d193d3b
Ahmet Gençoğlu <agncglu@gmail.com>
2026-03-26 14:39:38 +0300

    CLOUD-826

---
+++ data_sync/data_sync/doctype/event_server/test_event_server.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, CloudGCS Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEventServer(FrappeTestCase):
+	pass
